a4780ed1afced6621085e44c4760cd39eaf34ec6
Dominik Kuc <dominik.kuc@microsoft.wsei.edu.pl>
2019-06-22 16:36:11 +0200

    commit up

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,5 @@
 (function () {
-    'use strict';
+    'use strict'; /* ograniczona wersja JS'a */
 
     var draggedEl,
         onDragStart,
@@ -20,39 +20,39 @@
     onDragStart = function (ev) {
         var boundingClientRect;
         if (ev.target.className.indexOf('bar') === -1) {
-            return;
+            return; /* sprawdzam czy element za ktory chce przeciagnac ma klase bar, jak nie posiada to nie wywołuję */
         }
 
-        draggedEl = this;
+        draggedEl = this; /* jeżeli chwiciliśmy za bar, przypisuje this'a teraz odwołuje się do sticker'a */
 
-        boundingClientRect = draggedEl.getBoundingClientRect();
+        boundingClientRect = draggedEl.getBoundingClientRect(); /* pozycja w przegladarce danego elementu */
 
-        grabPointY = boundingClientRect.top - ev.clientY;
+        grabPointY = boundingClientRect.top - ev.clientY; /* pozycja od góry i od lewej strony */
         grabPointX = boundingClientRect.left - ev.clientX;
     };
 
     onDrag = function (ev) {
-        if (!draggedEl) {
+        if (!draggedEl) { /* jesl nie nie przeciagamy zadnego elementu funkcji nie wywołujemy */
             return;
         }
 
-        var posX = ev.clientX + grabPointX,
+        var posX = ev.clientX + grabPointX, /* przesuniecie, nowa pozycja notesu */
             posY = ev.clientY + grabPointY;
 
         if (posX < 0) {
-            posX = 0;
+            posX = 0; /* blokowanie przeciągniecia poza okno przeglądarki poz. X oraz poz. Y */
         }
 
         if (posY < 0) {
             posY = 0;
         }
 
-        draggedEl.style.transform = "translateX(" + posX + "px) translateY(" + posY + "px)";
+        draggedEl.style.transform = "translateX(" + posX + "px) translateY(" + posY + "px)"; /* przesuwanie na translat'ach, przesuwanie o pozycję */
     };
 
     onDragEnd = function () {
         draggedEl = null;
-        grabPointX = null;
+        grabPointX = null; /* zerujemy wszystkie punkty */
         grabPointY = null;
     };
 
@@ -76,13 +76,13 @@
     createNote = function (options) {
         var stickerEl = document.createElement('div'),
             barEl = document.createElement('div'),
-            saveBtnEl = document.createElement('button'),
+            saveBtnEl = document.createElement('button'), /* definiowanie elementów notatki tak by nie towrzyć jedne tylko ile chcemy */
             deleteBtnEl = document.createElement('button'),
             textareaEl = document.createElement('textarea'),
-            BOUNDARIES = 400,
+            BOUNDARIES = 500,
             noteConfig = options || {
                 transformCSSValue: "translateX(" + Math.random() * BOUNDARIES + "px) translateY(" + Math.random() * BOUNDARIES + "px)",
-                content: '',
+                content: '', /* losowość miejsca tworzenia nowe notatki */
                 id: "sticker_" + new Date().getTime(),
             },
             onSave,
@@ -91,75 +91,77 @@
         if (noteConfig.textarea) {
             textareaEl.style.width = noteConfig.textarea.width;
             textareaEl.style.height = noteConfig.textarea.height;
-            textareaEl.style.resize = 'none';
+            textareaEl.style.resize = 'both';
         }
 
         onSave = function () {
             saveNote(
-                getNoteObject(stickerEl)
+                getNoteObject(stickerEl) /* wywołanie fucnkji save czyli zapisania notatki */
             );
         };
 
         onDelete = function () {
             deleteNote(
-                getNoteObject(stickerEl)
+                getNoteObject(stickerEl) /* wywołanie funkcji delete czyli usuniecia notatki */
             );
 
             document.body.removeChild(stickerEl);
         };
 
-        stickerEl.style.transform = noteConfig.transformCSSValue;
+        stickerEl.style.transform = noteConfig.transformCSSValue; /* przypisanie wartości aby była ta notka w losowym miesjcu */
         stickerEl.id = noteConfig.id;
         textareaEl.value = noteConfig.content;
 
-        saveBtnEl.classList.add('saveButton');
+        saveBtnEl.innerText = 'S';
+        saveBtnEl.classList.add('saveButton'); /* dodawanie klasy do elementu utworzonego przez  funkcje create element */
         saveBtnEl.addEventListener('click', onSave, false);
 
+        deleteBtnEl.innerText = 'X';
         deleteBtnEl.classList.add('deleteButton');
         deleteBtnEl.addEventListener('click', onDelete, false);
 
         barEl.classList.add('bar');
         stickerEl.classList.add('sticker');
 
-        barEl.appendChild(saveBtnEl);
+        barEl.appendChild(saveBtnEl); /* dodanie buttonów do bar'u */
         barEl.appendChild(deleteBtnEl);
 
-        stickerEl.appendChild(barEl);
+        stickerEl.appendChild(barEl); /* dopisywanie do sticekerel odpowwiednich rzeczy ktore w nic chcemy miec */
         stickerEl.appendChild(textareaEl);
 
         stickerEl.addEventListener('mousedown', onDragStart, false);
 
-        document.body.appendChild(stickerEl);
+        document.body.appendChild(stickerEl); /* to wszystko ma sie znajdować w body */
     };
 
     testLocalStorage = function () {
         var foo = 'foo';
         try {
             localStorage.setItem(foo, foo);
-            localStorage.removeItem(foo);
-            return true;
+            localStorage.removeItem(foo); /* funkacja która sprawdza czy możemy użyć localstorage */
+            return true; 
         } catch (e) {
             return false;
         }
     };
 
-    init = function () {
+    init = function () { /* funkcja która wywołuje się przy uruchomieniu programu */
 
         if (!testLocalStorage) {
-            var message = "We are sorry but you cannot use localStorage";
+            var message = "We are sorry but you cannot use localStorage"; /* sprawdzam czy testlocalstorage wyrzucił nam jakiś błąd */
             saveNote = function () {
-                console.warn(message);
+                console.warn(message); 
             };
 
             deleteNote = function () {
                 console.warn(message);
             };
-        } else {
+        } else { /* jeżeli localstroge jest ok */
             saveNote = function (note) {
-                localStorage.setItem(note.id, JSON.stringify(note));
+            localStorage.setItem(note.id, JSON.stringify(note));
             };
 
-            deleteNote = function (note) {
+            deleteNote = function (note) { /* ładujemy wszsytkie te 3 funkcje */
                 localStorage.removeItem(note.id);
             };
 
@@ -177,11 +179,110 @@
         }
 
         addNoteBtnEl = document.querySelector('.addNoteBtn');
-        addNoteBtnEl.addEventListener('click', onAddNoteBtnClick, false);
-        document.addEventListener('mousemove', onDrag, false);
-        document.addEventListener('mouseup', onDragEnd, false);
+        addNoteBtnEl.addEventListener('click', onAddNoteBtnClick, false); /* nasłuchwianie przycisku na button tworzenia notatki i jego wywoływanie */
+        document.addEventListener('mousemove', onDrag, false); /* nasłuchiwanie na zdarzenie, poruszam myszka i ona sie wywołuje */
+        document.addEventListener('mouseup', onDragEnd, false); /* gdy puszczamy myszkę wywołujemy ondrag end */
     };
 
     init();
+
+    function clock() {
+        var now = new Date();
+        var ctx = document.getElementById('canvas').getContext('2d');
+        ctx.save();
+        ctx.clearRect(0, 0, 150, 150);
+        ctx.translate(75, 75);
+        ctx.scale(0.4, 0.4);
+        ctx.rotate(-Math.PI / 2);
+        ctx.strokeStyle = 'black';
+        ctx.fillStyle = 'white';
+        ctx.lineWidth = 8;
+        ctx.lineCap = 'round';
+      
+        // Hour marks
+        ctx.save();
+        for (var i = 0; i < 12; i++) {
+          ctx.beginPath();
+          ctx.rotate(Math.PI / 6);
+          ctx.moveTo(100, 0);
+          ctx.lineTo(120, 0);
+          ctx.stroke();
+        }
+        ctx.restore();
+      
+        // Minute marks
+        ctx.save();
+        ctx.lineWidth = 5;
+        for (i = 0; i < 60; i++) {
+          if (i % 5!= 0) {
+            ctx.beginPath();
+            ctx.moveTo(117, 0);
+            ctx.lineTo(120, 0);
+            ctx.stroke();
+          }
+          ctx.rotate(Math.PI / 30);
+        }
+        ctx.restore();
+       
+        var sec = now.getSeconds();
+        var min = now.getMinutes();
+        var hr  = now.getHours();
+        hr = hr >= 12 ? hr - 12 : hr;
+      
+        ctx.fillStyle = 'black';
+      
+        // write Hours
+        ctx.save();
+        ctx.rotate(hr * (Math.PI / 6) + (Math.PI / 360) * min + (Math.PI / 21600) *sec);
+        ctx.lineWidth = 14;
+        ctx.beginPath();
+        ctx.moveTo(-20, 0);
+        ctx.lineTo(80, 0);
+        ctx.stroke();
+        ctx.restore();
+      
+        // write Minutes
+        ctx.save();
+        ctx.rotate((Math.PI / 30) * min + (Math.PI / 1800) * sec);
+        ctx.lineWidth = 10;
+        ctx.beginPath();
+        ctx.moveTo(-28, 0);
+        ctx.lineTo(112, 0);
+        ctx.stroke();
+        ctx.restore();
+       
+        // Write seconds
+        ctx.save();
+        ctx.rotate(sec * Math.PI / 30);
+        ctx.strokeStyle = '#D40000';
+        ctx.fillStyle = '#D40000';
+        ctx.lineWidth = 6;
+        ctx.beginPath();
+        ctx.moveTo(-30, 0);
+        ctx.lineTo(83, 0);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.arc(0, 0, 10, 0, Math.PI * 2, true);
+        ctx.fill();
+        ctx.beginPath();
+        ctx.arc(95, 0, 10, 0, Math.PI * 2, true);
+        ctx.stroke();
+        ctx.fillStyle = 'rgba(0, 0, 0, 0)';
+        ctx.arc(0, 0, 3, 0, Math.PI * 2, true);
+        ctx.fill();
+        ctx.restore();
+      
+        ctx.beginPath();
+        ctx.lineWidth = 14;
+        ctx.strokeStyle = '#696e9e';
+        ctx.arc(0, 0, 142, 0, Math.PI * 2, true);
+        ctx.stroke();
+      
+        ctx.restore();
+      
+        window.requestAnimationFrame(clock);
+      }
+
+      window.requestAnimationFrame(clock);
 
 })();
